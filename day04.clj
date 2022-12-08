cfc0e8d6b029f97a177505ca04eb84d31aa08f89
Sergey Larionov <sergeylarionov@cloud.upwork.com>
2022-12-08 22:08:20 +0700

    day  04 clojure

---
+++ day04.clj
@@ -0,0 +1,25 @@
+(ns day03
+  (:require [utils :as u]
+            [hashp.core]
+            [clojure.string :as str]
+            [com.rpl.specter :as sp]
+            [clojure.java.io :as io]))
+
+(u/download-data 2022 4)
+
+(def data (u/load-data "day04.dat"))
+(def data "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8")
+
+data
+(->> (str/split-lines data)
+     (map #(str/split % #","))
+     (sp/transform [sp/ALL sp/ALL] #(str/split % #"-"))
+     (sp/transform [sp/ALL sp/ALL sp/ALL] u/char->int)
+     (map #(list ((list (first (first %)) (last %) (last (first %))))
+                 (list (first (first %)) (last %) (last (first %))))))
+     ;;(map flatten))
--- day04_1.js
+++ day04_1.js
@@ -1,20 +1,14 @@
-console.log("---");
-import { readLines } from "https://deno.land/std@0.116.0/io/bufio.ts";
-import { range } from "https://deno.land/x/it_range@v1.0.3/range.mjs";
-import { slidingWindows } from "https://deno.land/std@0.116.0/collections/mod.ts";
-import count from "https://deno.land/x/denodash@0.1.3/src/collection/count.ts";
+console.log('---');
+import { readLines } from 'https://deno.land/std@0.116.0/io/bufio.ts';
+import { range } from 'https://deno.land/x/it_range@v1.0.3/range.mjs';
+import { slidingWindows } from 'https://deno.land/std@0.116.0/collections/mod.ts';
+import count from 'https://deno.land/x/denodash@0.1.3/src/collection/count.ts';
 
-// dconst all = [];
-// for await (let line of readLines(Deno.stdin)) {
-//   console.log({ line });
-//   all.push(line.split("").map((v) => parseInt(v, 10)));
-// }
-
-const all = (await Deno.readTextFile("./resources/input04.txt"))
+const all = (await Deno.readTextFile('./resources/input04.txt'))
   .trim()
-  .split("\n")
+  .split('\n')
   .map((s) =>
-    s.split(",").map((s) => s.split("-").map((i) => parseInt(i, 10)))
+    s.split(',').map((s) => s.split('-').map((i) => parseInt(i, 10))),
   );
 
 const contains = ([[a, b], [x, y]]) => (x >= a && y <= b) || (a >= x && b <= y);
@@ -25,5 +19,5 @@ for (let p of all) {
 console.log(
   { all },
   all.filter(contains2),
-  all.length - all.filter(contains2).length
+  all.length - all.filter(contains2).length,
 );
