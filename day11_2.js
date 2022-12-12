84388a709f2df6d172c94ed89f6657abe7426ecb
Sergey Larionov <sergeylarionov@cloud.upwork.com>
2022-12-12 16:59:49 +0300

    day 11 and 12

---
+++ day11_2.js
@@ -0,0 +1,58 @@
+console.log('\n\n\n\n###################################');
+import { readLines } from 'https://deno.land/std@0.116.0/io/bufio.ts';
+import { range } from 'https://deno.land/x/it_range@v1.0.3/range.mjs';
+import { slidingWindows } from 'https://deno.land/std@0.116.0/collections/mod.ts';
+import count from 'https://deno.land/x/denodash@0.1.3/src/collection/count.ts';
+import { writeAllSync } from 'https://deno.land/std/streams/conversion.ts';
+
+let input = await Deno.readTextFile('./resources/input11.txt');
+//input = await Deno.readTextFile('./resources/input11.txt');
+// input = `noop
+// addx 3
+// addx -5`;
+
+const mem = {};
+let d = 1;
+const prg = input
+  .trim()
+  .split('\n\n')
+  .map((s) => s.split('\n'))
+  .map((s) => {
+    const m = {};
+    m.id = parseInt(s[0].match(/\d+/)[0], 10);
+    m.items = s[1]
+      .split(':')[1]
+      .trim()
+      .split(', ')
+      .map((i) => parseInt(i, 10));
+    m.op = (old) => {
+      const exp = s[2].split('= ')[1].replace('old', old);
+      return eval(exp);
+    };
+    s[3] = parseInt(s[3].match(/\d+/), 10);
+    s[4] = parseInt(s[4].match(/\d+/), 10);
+    s[5] = parseInt(s[5].match(/\d+/), 10);
+    m.cnt = 0;
+    d *= s[3];
+    m.test = (v) => (v % s[3] ? s[5] : s[4]);
+    return m;
+  });
+
+console.log({ prg });
+
+for (let i = 0; i < 10000; i++) {
+  console.log('Round: ', i + 1);
+  for (let m = 0; m < prg.length; m++) {
+    const mn = prg[m];
+    mn.cnt += mn.items.length;
+    while (mn.items.length) {
+      const it = mn.items.shift();
+      let newIt = mn.op(it) % d;
+      //console.log({ it, newIt }, mn.test(newIt));
+      prg[mn.test(newIt)].items.push(newIt);
+    }
+  }
+  //console.log({ prg });
+}
+const srt = prg.sort((a, b) => b.cnt - a.cnt);
+console.log({ srt }, srt[0].cnt * srt[1].cnt);
---
+++ day12.js
@@ -0,0 +1,76 @@
+console.log('\n\n\n\n###################################');
+import { readLines } from 'https://deno.land/std@0.116.0/io/bufio.ts';
+import { range } from 'https://deno.land/x/it_range@v1.0.3/range.mjs';
+import { slidingWindows } from 'https://deno.land/std@0.116.0/collections/mod.ts';
+import count from 'https://deno.land/x/denodash@0.1.3/src/collection/count.ts';
+import { writeAllSync } from 'https://deno.land/std/streams/conversion.ts';
+
+let input = await Deno.readTextFile('./resources/input12.txt');
+//input = await Deno.readTextFile('./resources/input12_demo.txt');
+
+const el = (c) => c.charCodeAt(0);
+const dist = (a, b) => a.charCodeAt(0) - b.charCodeAt(0);
+
+const nei = (i, j, w, h) =>
+  [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+  ]
+    .map(([dy, dx]) => [i + dy, j + dx])
+    .filter(([i, j]) => {
+      if (i < 0 || j < 0) return false;
+      if (i > h - 1 || j > w - 1) return false;
+      return true;
+    });
+const get = (prg, [i, j]) => prg[i][j];
+const mem = {};
+let d = 1;
+const prg = input
+  .trim()
+  .split('\n')
+  .map((s) => s.split(''));
+
+let start = [0, 0];
+let end = [0, 0];
+let h = prg.length,
+  w = prg[0].length;
+for (let r = 0; r < prg.length; r++) {
+  for (let c = 0; c < prg[0].length; c++) {
+    if (prg[r][c] === 'S') {
+      start = [r, c];
+    }
+    if (prg[r][c] === 'E') {
+      end = [r, c];
+    }
+  }
+}
+
+const q = [[start, 0, {}]];
+const cache = {};
+
+const res = [];
+
+while (q.length) {
+  q.sort((a, b) => cache[b[0].join('-')] - cache[a[0].join('-')]);
+  const top = q.pop();
+
+  const [el, len] = top;
+  const neis = nei(...el, w, h);
+
+  for (let ch of neis) {
+    let dst = dist(get(prg, ch), get(prg, el));
+    if (get(prg, ch) === 'E') {
+      dst = dist('z', get(prg, el));
+    }
+    if (get(prg, el) === get(prg, ch) || get(prg, el) === 'S' || dst <= 1) {
+      if (!cache[ch.join('-')] || cache[ch.join('-')] > len + 1) {
+        cache[ch.join('-')] = len + 1;
+        q.push([ch, len + 1]);
+      }
+    }
+  }
+}
+
+console.log(cache[end.join('-')]);
